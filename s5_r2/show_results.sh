0ade891b36c7641cd2540daffa9b377fc2970909
milde <bmilde@gmail.com>
2018-06-04 17:45:53 +0200

    added show results script

--- s5_r2/show_results.sh
+++ s5_r2/show_results.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-for x in exp/{mono,tri,sgmm,nnet,dnn,lstm}*/decode*; do [ -d $x ] && [[ $x =~ "$1" ]] && grep WER $x/wer* | utils/best_wer.sh; done 2>/dev/null
-for x in exp/chain_cleaned/tdnn*/decode*; do [ -d $x ] && [[ $x =~ "$1" ]] && grep WER $x/wer* | utils/best_wer.sh; done 2>/dev/null
+exp_dir=exp/
+for x in ${exp_dir}/{mono,tri,sgmm,nnet,dnn,lstm}*/decode*; do [ -d $x ] && [[ $x =~ "$1" ]] && grep WER $x/wer* | utils/best_wer.sh; done 2>/dev/null
+for x in ${exp_dir}/chain_cleaned/tdnn*/decode*; do [ -d $x ] && [[ $x =~ "$1" ]] && grep WER $x/wer* | utils/best_wer.sh; done 2>/dev/null
 exit 0
