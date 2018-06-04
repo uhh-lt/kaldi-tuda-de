bd081308d31900f74c9bce2e917138001d78cccf
milde <bmilde@gmail.com>
2018-06-04 17:19:07 +0200

    changed default jobs

--- s5_r2/cmd.sh
+++ s5_r2/cmd.sh
@@ -15,5 +15,5 @@ export train_cmd="utils/run.pl"
 export decode_cmd="utils/run.pl"
 export cuda_cmd="utils/run.pl -l gpu=1"
 
-export nJobs=16
-export nDecodeJobs=5
+export nJobs=28
+export nDecodeJobs=12
