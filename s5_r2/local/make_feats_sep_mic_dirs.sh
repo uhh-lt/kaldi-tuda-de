abebe66296f56365606a3e123b4b0fc5e246a4d4
milde <bmilde@gmail.com>
2018-10-10 15:17:00 +0200

    added hires features

--- s5_r2/local/make_feats_sep_mic_dirs.sh
+++ s5_r2/local/make_feats_sep_mic_dirs.sh
@@ -4,6 +4,7 @@
 # This creates features for serparates microphone dirs, useful if you want to separate WERs for the different microphones in tuda dev / test
 
 for x in dev_a dev_b dev_c dev_d dev_e test_a test_b test_c test_d test_e; do
+  echo "Creating features for: $x"
   utils/fix_data_dir.sh data/$x # some files fail to get mfcc for many reasons
   steps/make_mfcc.sh --cmd "$train_cmd" --nj $nJobs data/$x exp/make_mfcc/$x $mfccdir
   utils/fix_data_dir.sh data/$x # some files fail to get mfcc for many reasons
@@ -11,3 +12,10 @@ for x in dev_a dev_b dev_c dev_d dev_e test_a test_b test_c test_d test_e; do
   utils/fix_data_dir.sh data/$x
 done
 
+for datadir in dev_a dev_b dev_c dev_d dev_e test_a test_b test_c test_d test_e; do
+  echo "Creating features for: ${datadir}_hires"
+  utils/copy_data_dir.sh data/$datadir data/${datadir}_hires
+  steps/make_mfcc.sh --nj $nJobs --mfcc-config conf/mfcc_hires.conf --cmd "$train_cmd" data/${datadir}_hires
+  steps/compute_cmvn_stats.sh data/${datadir}_hires
+  utils/fix_data_dir.sh data/${datadir}_hires
+done
