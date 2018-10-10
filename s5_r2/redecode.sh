fffe42e35373c5e3a6d9f9ff1f8618e2a89aaffd
milde <bmilde@gmail.com>
2018-10-10 15:15:28 +0200

    expanded redecode script (redecode with different decoding FSTs)

--- s5_r2/redecode.sh
+++ s5_r2/redecode.sh
@@ -19,30 +19,56 @@
 
 stage=0
 nnet3_affix=_cleaned
-dir=exp/chain_cleaned/tdnn1f_sp_bi/
+
+gmmdir=exp/tri4_cleaned
+dir=exp/chain_cleaned/tdnn1f_1024_sp_bi/
+#dir=exp/chain_cleaned/tdnn7o_std_sp_bi/
+
+gmm_decode_stage=0
+tdnn_decode_stage=0
+
+decode_affix=_300k3
+#langdir=data/lang_test_pron
+lang_dir=data/lang_300k3_test_300k3
+graph_dir=$gmmdir/graph_pron${decode_affix}
+
+# uncomment these if you would like to rescore only
+# gmm_decode_stage=6
+# tdnn_decode_stage=3
 
 . utils/parse_options.sh
 
+
 if [ $stage -le 1 ]; then
+  #Usage: utils/mkgraph.sh [options] <lang-dir> <model-dir> <graphdir>
 
-  graph_dir=exp/tri4/graph_pron
-  for dset in dev test; do
-      echo "Now decoding $dset with GMM-HMM model" 
-      steps/decode_fmllr.sh --nj $nDecodeJobs --cmd "$decode_cmd" --config conf/decode.config \
-                   $graph_dir data/${dset} exp/tri4/decode_${dset}_pron
-  done
-
+  #$train_cmd $graph_dir/mkgraph.log \
+  utils/mkgraph.sh $lang_dir $gmmdir $graph_dir
 fi
 
 if [ $stage -le 2 ]; then
+for dset in dev test dev_a dev_b dev_c dev_d test_a test_b test_c test_d; do
+#  for dset in dev test; do
+      echo "Now decoding $dset with GMM-HMM model" 
+      steps/decode_fmllr.sh --nj $nDecodeJobs --num-threads 2 --cmd "$decode_cmd" --config conf/decode.config --stage $gmm_decode_stage \
+                   $graph_dir data/${dset} $gmmdir/decode${decode_affix}_${dset}_pron
+  done
+fi
 
-  for dset in dev test; do
+if [ $stage -le 3 ]; then
+  utils/mkgraph.sh --self-loop-scale 1.0 $lang_dir $dir $dir/graph${decode_affix}
+fi
+
+if [ $stage -le 4 ]; then
+for dset in dev_e test_e; do
+# for dset in dev_a dev_b dev_c dev_d test_a test_b test_c test_d; do
+#  for dset in dev test; do
       echo "Now decoding $dset with TDNN-HMM model"
-      steps/nnet3/decode.sh --num-threads 4 --nj $nDecodeJobs --cmd "$decode_cmd" \
+      steps/nnet3/decode.sh --num-threads 4 --nj $nDecodeJobs --cmd "$decode_cmd" --stage $tdnn_decode_stage \
           --acwt 1.0 --post-decode-acwt 10.0 \
           --online-ivector-dir exp/nnet3${nnet3_affix}/ivectors_${dset}_hires \
           --scoring-opts "--min-lmwt 5 " \
-         $dir/graph data/${dset}_hires $dir/decode_${dset} || exit 1;
+         $dir/graph${decode_affix} data/${dset}_hires $dir/decode${decode_affix}_${dset} || exit 1;
       #  steps/lmrescore_const_arpa.sh --cmd "$decode_cmd" data/lang data/lang_rescore \
       #    data/${dset}_hires ${dir}/decode_${dset} ${dir}/decode_${dset}_rescore || exit 1
   done
