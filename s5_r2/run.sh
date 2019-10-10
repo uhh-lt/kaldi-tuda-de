e1cbc18bc88e05b001ffaad27f15126515879e89
patientzero <me@sebastianbayerl.de>
2019-10-10 10:48:41 +0200

    add exit on error to avoid follow up errors

--- s5_r2/run.sh
+++ s5_r2/run.sh
@@ -16,6 +16,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+set -e # exit on error
 
 stage=0
 use_BAS_dictionaries=false
