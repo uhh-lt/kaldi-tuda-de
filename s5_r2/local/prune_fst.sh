c99a33e91c9aa0fd49cd5ffdf4e2be40ab962362
bmilde <bmilde@gmail.com>
2022-04-10 18:51:22 +0200

    typo

--- s5_r2/local/prune_fst.sh
+++ s5_r2/local/prune_fst.sh
@@ -7,6 +7,6 @@ prune_factor=100.0
 
 export PATH=$KALDI_ROOT/tools/kaldi_lm:$PATH
 
-prune_lm.sh --arpa $prune_factor data/local/lm_${dict_suffix}/4gram-mincount/
-./local/format_data.sh --arpa_lm data/local/lm_${dict_suffix}/4gram-mincount/lm_pr${prune_factor}.gz --lang_in_dir data/lang_${dict_suffix} --lang_out_dir data/lang_${dict_suffix}_pr${prune_factor}_test
-./local/run_tdnn_1f_B.sh --stage 19 --lang-dir data/lang_${dict_suffix}_pr${prune_factor} --decode-affix _pr${prune_factor}
+prune_lm.sh --arpa $prune_factor data/local/lm${dict_suffix}/4gram-mincount/
+./local/format_data.sh --arpa_lm data/local/lm${dict_suffix}/4gram-mincount/lm_pr${prune_factor}.gz --lang_in_dir data/lang${dict_suffix} --lang_out_dir data/lang_${dict_suffix}_pr${prune_factor}_test
+./local/run_tdnn_1f_B.sh --stage 19 --lang-dir data/lang${dict_suffix}_pr${prune_factor} --decode-affix _pr${prune_factor}
