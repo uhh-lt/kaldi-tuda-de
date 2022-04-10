991f907df1e2f4507404f97695335c1f9348132d
bmilde <bmilde@gmail.com>
2022-04-10 18:50:30 +0200

    better prune fst script

--- s5_r2/local/prune_fst.sh
+++ s5_r2/local/prune_fst.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+dict_suffix=_std_big_v6
+prune_factor=100.0
+
 . ./path.sh || exit 1; # for KALDI_ROOT                                                                                                                                                           
 
 export PATH=$KALDI_ROOT/tools/kaldi_lm:$PATH
 
-prune_lm.sh --arpa 200.0 data/local/lm_std_big_v5/4gram-mincount/
-./local/format_data.sh --arpa_lm data/local/lm_std_big_v5/4gram-mincount/lm_pr200.0.gz --lang_in_dir data/lang_std_big_v5 --lang_out_dir data/lang_std_big_v5_pr200_test
-./local/run_tdnn_1f_B.sh --stage 19 --lang-dir data/lang_std_big_v5_pr200 --decode-affix _pr200
+prune_lm.sh --arpa $prune_factor data/local/lm_${dict_suffix}/4gram-mincount/
+./local/format_data.sh --arpa_lm data/local/lm_${dict_suffix}/4gram-mincount/lm_pr${prune_factor}.gz --lang_in_dir data/lang_${dict_suffix} --lang_out_dir data/lang_${dict_suffix}_pr${prune_factor}_test
+./local/run_tdnn_1f_B.sh --stage 19 --lang-dir data/lang_${dict_suffix}_pr${prune_factor} --decode-affix _pr${prune_factor}
